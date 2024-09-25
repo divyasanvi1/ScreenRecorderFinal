db7308a31ae6047ed6dc4723525231b1bd3b904b
divyasanvi1 <sanvidiv22@gmail.com>
2024-09-26 01:56:17 +0530

    conf

--- src/library/conf.js
+++ src/library/conf.js
@@ -5,8 +5,6 @@ const conf = {
     databaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
     collectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
     apiKey:String(import.meta.env.VITE_APPWRITE_API_KEY),
-    redirectUrl: import.meta.env.MODE === 'development'
-        ? import.meta.env.VITE_APPWRITE_REDIRECT_URL_LOCAL
-        : import.meta.env.VITE_APPWRITE_REDIRECT_URL_DEPLOYED,
+    redirectUrl: import.meta.env.VITE_APPWRITE_REDIRECT_URL_DEPLOYED,
 }
 export default conf;
